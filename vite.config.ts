e4bbbd81d95edda5f158ea0354ba75035b50b7ed
k-miyoshi <miyoshiez22@gmail.com>
2025-04-04 21:43:53 +0900

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  build: {
+    outDir: './docs',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
+  define: {
+    global: 'window',
+  },
+})
